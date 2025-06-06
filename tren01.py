55ce971ec232b520e075ad0934b7f43bfaaf7d17
Lazaro-Costta <lasarocosta11@gmail.com>
2025-06-06 15:47:04 -0300

    dia dia

---
+++ tren01.py
@@ -0,0 +1,5 @@
+dados = {'nome':'', 'media':'' }
+dados['nome'] = str(input('Nome:'))
+dados['media'] = float(input(f'media de {dados['nome']}'))
+print(f'o nome é igual a {dados['nome']}')
+print(f'a media é igual a {dados["media"]}')
