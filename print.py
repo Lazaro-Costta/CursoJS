dc435e65cafcb67a7b95e7ee941aa1ea08fb11c0
Lazaro-Costta <lasarocosta11@gmail.com>
2025-09-02 12:22:35 -0300

    y

--- print.py
+++
@@ -1,6 +0,0 @@
-print('ola mundo')
-nu = []
-for c in range(1, 5):
-    n = int(input(f'\033[31mdigite o {c}° numero:'))
-    nu.append(n)
-print(f'menor numero foi {min(nu)}')
--- tren01.py
+++
@@ -1,9 +0,0 @@
-dados = {'nome':'', 'media':'' }
-dados['nome'] = str(input('Nome:'))
-dados['media'] = float(input(f'media de {dados['nome']}:'))
-print(f'o nome é igual a {dados['nome']}')
-print(f'a media é igual a {dados["media"]}')
-if dados['media'] < 6.5:
-    print('REPROVADO')
-else:
-    print('APROVADO')
